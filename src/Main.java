ff47cf77fea819783abb1ef30a6ef26990d02868
iev <iev_ak@mail.ru>
2023-08-05 20:57:51 +0300

    abc

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+class    uktam{
+    Main p21 = new Main();
+}
+
+
+
